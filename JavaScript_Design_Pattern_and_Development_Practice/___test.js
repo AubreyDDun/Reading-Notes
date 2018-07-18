e91386309335e56769c964ffcd079b2858522fc9
AubreyDDun <AubreyF@126.com>
2018-07-19 00:03:23 +0800

    test: submit a new file

---
+++ JavaScript_Design_Pattern_and_Development_Practice/___test.js
@@ -0,0 +1,16 @@
+function whatever(params) {
+    if (params) {
+        // do something
+        // line4
+        // line5
+        // line6
+        // line7
+        
+    } else{
+        // do something else
+        // line11
+        // line12
+        // line13
+        // line14
+    }
+}
